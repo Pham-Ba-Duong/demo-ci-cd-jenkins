ce175cc213d322b9c31af6502a3643f570496fb3
phambaduong <pham2008duong2004@gmail.com>
2025-10-15 07:35:13 +0700

    update style

--- index.js
+++ index.js
@@ -5,30 +5,27 @@ const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const PORT = 3000;
 
-// Cấu hình EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(expressLayouts);
 app.set("layout", "layout");
 
-// Cấu hình static
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-// Trang chính
 app.get("/", (req, res) => {
   res.render("index", { page: "Trang chủ" });
 });
 
-// Trang thư viện
 app.get("/gallery", (req, res) => {
   res.render("gallery", { page: "Thư viện" });
 });
 
-// Trang giới thiệu
 app.get("/about", (req, res) => {
   res.render("about", { page: "Giới thiệu" });
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server đang chạy tại http://localhost:${PORT}`);
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
+
+//             http://165.232.168.204:3000/
